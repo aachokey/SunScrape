994a7a0d7ad52d3465c099e418c9f9d7b05456b7
Aric Chokey <achokey21@gmail.com>
2019-03-18 12:01:04 -0400

    change naming

--- sunscrape/__init__.py
+++ sunscrape/__init__.py
@@ -1,7 +1,1 @@
 name = "sunscrape"
-
-from candidate import CandidateScraper
-from committee import CommitteeScraper
-from contributions import ContributionScraper
-from expenditures import ExpenditureScraper
-from transfers import TransferScraper
